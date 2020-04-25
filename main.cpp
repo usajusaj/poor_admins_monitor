5195e6c0259082661c9187c9b9c6e6ce92a4ef87
usajusaj <usajusaj@usajusaj.org>
2020-04-24 23:09:06 -0400

    Better main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <drogon/drogon.h>
 
-int main() {
+int main(int argc, char** argv) {
     //Load config file
-    drogon::app().loadConfigFile("../config.json");
+    std::string config = "../config.json";
+    if (argc == 2) {
+        config = argv[1];
+    }
+
+    drogon::app().loadConfigFile(config);
     drogon::app().run();
     return 0;
 }
